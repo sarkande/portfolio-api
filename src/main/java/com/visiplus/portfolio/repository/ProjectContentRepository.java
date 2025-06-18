90a562dc7fa2ed7389a9f01ded631896b6071cf8
sarkande <aperezgo74@gmail.com>
2025-06-18 22:15:34 +0200

    Add project content management

---
+++ src/main/java/com/visiplus/portfolio/repository/ProjectContentRepository.java
@@ -0,0 +1,7 @@
+package com.visiplus.portfolio.repository;
+
+import com.visiplus.portfolio.models.ProjectContent;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProjectContentRepository extends JpaRepository<ProjectContent, Long> {
+}
---
+++ src/main/java/com/visiplus/portfolio/services/ProjectContentService.java
@@ -0,0 +1,8 @@
+package com.visiplus.portfolio.services;
+
+import com.visiplus.portfolio.models.Project;
+import com.visiplus.portfolio.models.ProjectContent;
+
+public interface ProjectContentService {
+    ProjectContent create(Project project);
+}
