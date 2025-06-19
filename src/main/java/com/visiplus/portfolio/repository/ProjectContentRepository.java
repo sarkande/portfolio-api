bbbaa6dfbb52083402b4a3261c17423288563e53
Allan Pure <allan@pure-illusion.com>
2025-06-19 21:46:00 +0200

    Revert "Add project content management"
    This reverts commit 90a562dc7fa2ed7389a9f01ded631896b6071cf8.

--- src/main/java/com/visiplus/portfolio/repository/ProjectContentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.visiplus.portfolio.repository;
-
-import com.visiplus.portfolio.models.ProjectContent;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProjectContentRepository extends JpaRepository<ProjectContent, Long> {
-}
--- src/main/java/com/visiplus/portfolio/services/ProjectContentService.java
+++
@@ -1,8 +0,0 @@
-package com.visiplus.portfolio.services;
-
-import com.visiplus.portfolio.models.Project;
-import com.visiplus.portfolio.models.ProjectContent;
-
-public interface ProjectContentService {
-    ProjectContent create(Project project);
-}
